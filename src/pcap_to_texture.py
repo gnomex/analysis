1a2ae38d34a0d9ca8cc6b8f0fe6d0cb3b6676306
Kenner Kliemann <kenner.hp@gmail.com>
2017-10-30 19:53:48 -0200

    trying new grayscale mode P and scale images

--- src/pcap_to_texture.py
+++ src/pcap_to_texture.py
@@ -11,13 +11,15 @@ def do_magic(filename):
     packets = rdpcap(filename)
     # sessions = packets.sessions()
 
-    print("get packets")
+    # avoid empty pcap
+    if len(packets) < 1:
+        return
 
     for packet in packets:
         byte_array = list(map(ord, str(packet))) # list of numbers, such as [0xDE, 0xAD, 0xBE, 0xEF]
         DUMP.append(byte_array)
 
-        print("dumped")
+    print("dumped")
     # for session in sessions:
     #     for packet in sessions[session]:
     #         byte_array = list(map(ord, str(packet))) # list of numbers, such as [0xDE, 0xAD, 0xBE, 0xEF]
@@ -27,7 +29,8 @@ def do_magic(filename):
 
     # shape nrows x 32
     n = int(len(flattened)/32) + 1
-    tf = np.zeros((n,32), dtype=np.uint32)
+    # tf = np.zeros((n,32), dtype=np.uint8)
+    tf = np.zeros((n,32), dtype=np.uint8)
 
     i = 0
     j = 0
@@ -42,18 +45,37 @@ def do_magic(filename):
 
     print("imagifying")
     # print(tf.shape)
-    img = Image.fromarray(tf, 'RGB')
-    w, h = img.size
-    scale_by = 1
-    print("W {} and h {}".format(w, h))
+    # img = Image.fromarray(tf, mode='RGB')
+    # img = Image.fromarray(tf)
+    # 1 (1-bit pixels, black and white, stored with one pixel per byte)
+    # L (8-bit pixels, black and white)
+    # P (8-bit pixels, mapped to any other mode using a color palette)
+    # RGB (3x8-bit pixels, true color)
+    # RGBA (4x8-bit pixels, true color with transparency mask)
+    # CMYK (4x8-bit pixels, color separation)
+    # YCbCr (3x8-bit pixels, color video format)
+    # LAB (3x8-bit pixels, the L*a*b color space)
+    # HSV (3x8-bit pixels, Hue, Saturation, Value color space)
+    # I (32-bit signed integer pixels)
+    # F (32-bit floating point pixels)
 
-    newsize = (w * scale_by, h * scale_by)
-    img = img.resize(newsize)
-    img.save("../dump_as_imgs/pd-{}.png".format(os.path.basename(filename)))
+    try:
+        img = Image.fromarray(tf, mode='P')
+        # img = Image.fromarray(tf, mode='RGB')
+        w, h = img.size
+        scale_by = 10
+        print("W {} and h {}".format(w, h))
+
+        newsize = (w * scale_by, h * scale_by)
+        img = img.resize(newsize)
+        img.save("../dump_as_imgs/pd-{}.png".format(os.path.basename(filename)))
+    except Exception as e:
+        print("Something rot there {}, file {}".format(e, filename))
 
 # file = '../../Pcaps/arp-storm.pcap'
 # do_magic(file)
 
+# path = '../../Pcaps'
 path = '../../kenner-pcaps'
 
 for filename in glob.glob(os.path.join(path, '*.pcap')):
@@ -64,6 +86,5 @@ for filename in glob.glob(os.path.join(path, '*.pcap')):
         do_magic(filename)
 
     except Exception as e:
-        pp.pprint("Something rot there {}".format(e))
-
+        print("Something rot there {}, file {}".format(e, filename))
 
